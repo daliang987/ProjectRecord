37422da3b781bb9aee03980d15655540dd929dde
daliang987 <daliang987@126.com>
2020-03-24 14:27:04 +0800

    database gitignore

--- application/database.php
+++ application/database.php
@@ -11,18 +11,17 @@
 
 return [
     // 数据库类型
-    //'type'            => 'mysql',
-    'type'            => 'Sqlsrv',
+    'type'            => 'mysql',
     // 服务器地址
     'hostname'        => 'localhost',
     // 数据库名
     'database'        => 'ProjectRecord',
     // 用户名
-    'username'        => 'sa',
+    'username'        => 'root',
     // 密码
     'password'        => 'trsadmin',
     // 端口
-    'hostport'        => '1433',
+    'hostport'        => '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
